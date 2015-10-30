67982f895464a57c38efee1ebc12dfb3f3f086cc
root <root@training3.training3>
2015-10-30 01:19:48 -0700

    Update example

--- src/TableShow/main.js
+++ src/TableShow/main.js
@@ -21,10 +21,11 @@
 
         // 假設是 ajax 的過程
         getRows2() {
-            this.state.table.rows = [
-                { id: 12, name: "iPhone 5",   price: 199,   stocked: false   },
-                { id: 13, name: "iPhone 6",   price: 299,   stocked: true    },
-                { id: 14, name: "iPhone 6s",  price: 399,   stocked: true    },
+            this.state.table.heads = ['name','price','stocked','size'];
+            this.state.table.rows  = [
+                { id: 12, name: "iPhone 5",   price: 199,   stocked: false, size: 'small'  },
+                { id: 13, name: "iPhone 6",   price: 299,   stocked: true,  size: 'normal'  },
+                { id: 14, name: "iPhone 6s",  price: 399,   stocked: true,  size: 'normal'  },
             ];
             this.setState({"table": this.state.table});
         },
